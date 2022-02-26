2591f01b4808a8c638ada92540580872caf838ad
Austin Tinsley <aus@tins.dev>
2022-02-25 19:09:26 -0800

    cleanup

--- src/DexSprite.tsx
+++ src/DexSprite.tsx
@@ -3,19 +3,13 @@ import './DexSprite.css';
 import { IPokemonSpecies } from './models/models';
 
 export const DexSprite = ({ pokemon, handleClick, shiny }: {
-  pokemon?: IPokemonSpecies,
+  pokemon: IPokemonSpecies,
   handleClick?: () => void,
   shiny?: boolean,
-}) => pokemon ? (
+}) => (
   <li
     className={`sprite-container ${pokemon.types[0]}-primary ${pokemon.types[pokemon.types.length - 1]}-secondary`}
   >
-    <span className={`pokesprite pokemon ${pokemon.slug}${shiny ? ' shiny' : ''}`} onClick={handleClick}></span>
+    <span className={`pokesprite pokemon ${pokemon.slug}${shiny ? ' shiny' : ''}`} onClick={handleClick} title={pokemon.slug}></span>
   </li>
-) : (
-    <li
-      className='sprite-container mystery-primary mystery-secondary'
-    >
-      <span className='pokesprite pokemon ditto shiny'></span>
-    </li>
-  )
+);
